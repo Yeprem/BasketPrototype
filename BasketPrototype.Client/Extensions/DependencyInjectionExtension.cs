86debeae654034fc3022d94bd5ded603a72156f8
Yeprem Bozoğlanoğlu <yeprem.bozoglanoglu@viagogo.com>
2018-11-25 14:15:28 +0000

    adding dependency extension for client

---
+++ BasketPrototype.Client/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using BasketPrototype.Client.Services;
+using BasketPrototype.v1.Client;
+
+namespace BasketPrototype.Client.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void InjectDependencies(this IServiceCollection collection, IConfiguration configuration)
+        {
+            collection.AddSingleton<IBasketServiceClient, BasketServiceClient>()
+                .AddSingleton<IHttpRequestHandler, HttpRequestHandler>();
+        }
+    }
+}
